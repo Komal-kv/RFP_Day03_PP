b6a73f34b15b874eb78f7de93a03c974148c61eb
Komal <divyavairagade@gmail.com>
2022-04-14 15:51:00 +0530

    Program added

--- AssignPP01/Class1.cs
+++ AssignPP01/Class1.cs
@@ -8,6 +8,23 @@ namespace AssignPP01
 {
     internal class Class1
     {
+        public static void LineComparison()
+        {
+            int x1, x2, y1, y2;
+            Console.WriteLine("enter x1 num= ");
+            x1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter y1 num= ");
+            y1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter x2 num= ");
+            x2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter y2 num= ");
+            y2 = int.Parse(Console.ReadLine());
+
+            var line1 = Math.Sqrt((Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)));
+            Console.WriteLine("Length Of Line1 is : " + line1);
+
+        }
 
     }
+
 }
--- AssignPP01/Program.cs
+++ AssignPP01/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome To Line Comparison Computation Program");
+AssignPP01.Class1.LineComparison();
